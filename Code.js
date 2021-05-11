678fcd302d035724de663d0c413903c47fd52633
navigit56 <78213785+navigit56@users.noreply.github.com>
2021-05-11 16:55:48 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,41 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+
+function clues(){
+
+    fill("white")
+      textSize(15)
+      text("A T A D A S B A", 500,70);
+      fill("lightblue")
+      text("Hint: A place where data can be stored !!", 100,70)
+
+
+ 
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 500,99)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,99)
+ 
+ 
+    fill("white")
+    textSize(15)
+    text("C T F U N O N I ", 640,130);
+    fill("lightblue")
+    text("Hint: A set of commands or instructions given to the computer for execution !!", 100,130)
+    
+}
+
+
+    
+
+
+
+
+   
+  
+    
+    
+
